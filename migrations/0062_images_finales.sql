ed2ea656bf87f6b47c257bb247a5d6efe69f1daf
tardenoluwamayowa96-coder <tardenoluwamayowa96@gmail.com>
2026-09-23 16:37:19 +0000

    Create 0062_images_finales.sql

---
+++ migrations/0062_images_finales.sql
@@ -0,0 +1,14 @@
+UPDATE products SET image_url = '/products/09_0805_Relief_sun_EU_UK_f73e0d53-3f10-4939-b886-734f50e4d51c.jpg' WHERE slug = 'beauty-of-joseon-relief-sun';
+UPDATE products SET image_url = '/products/MEDICUBE_Deep_Vita_C_Capsule_Serum_-_01.png' WHERE slug = 'medicube-deep-vita-c-serum';
+UPDATE products SET image_url = '/products/skin1004-hyalu-cica-water-fit-sun-serum-uv-1233347068.jpg' WHERE slug = 'skin1004-hyalu-cica-sun-serum';
+UPDATE products SET image_url = '/products/laneige-lip-sleeping-mask (1).webp' WHERE slug = 'laneige-lip-sleeping-mask-mini';
+UPDATE products SET image_url = '/products/9a1a280bd5a3ca4f3c9fc6bba2e35d0e.png' WHERE slug = 'mixsoon-bean-essence';
+UPDATE products SET image_url = '/products/1705555626241_c395e97813374d3c93a9a5b8cbcb3826.jpg' WHERE slug = 'haruharu-airyfit-sunscreen';
+UPDATE products SET image_url = '/products/Haruharu_Wonder_Black_Rice_Hyaluronic_Toner_Fragrance_Free_150ml.png' WHERE slug = 'haruharu-hyaluronic-toner';
+UPDATE products SET image_url = '/products/IMG_5126.png' WHERE slug = 'anua-niacinamide-10-txa-4-serum';
+UPDATE products SET image_url = '/products/Beauty-of-Joseon-Glow-Deep-Serum-Rice-and-Alpha-Arbutin-pigmentfolt-halvanyito-szerum.png' WHERE slug = 'beauty-of-joseon-glow-deep-serum';
+UPDATE products SET image_url = '/products/6_69eb6d81-7b57-4af2-b6e1-2eaf296304e1.png' WHERE slug = 'skin1004-centella-ampoule-foam';
+UPDATE products SET image_url = '/products/jar_-07.jpg' WHERE slug = 'torriden-dive-in-cream';
+UPDATE products SET image_url = '/products/eos-body-wash-Vanilla-Cashmere.jpg' WHERE slug = 'eos-body-wash-vanilla';
+UPDATE products SET image_url = '/products/eos-shea-better-cashmere-body-wash-coconut-waters-70052923343231.png' WHERE slug = 'eos-body-wash-coconut';
+UPDATE products SET image_url = '/products/assets%252Fimages%252F1%2520packs%2520ks-kojisansoapx3-100-adam2019%2520%25281%2529__50015.1743493110.386.513__78787.1756205883.jpg' WHERE slug = 'kojie-san-soap-pack-3';
